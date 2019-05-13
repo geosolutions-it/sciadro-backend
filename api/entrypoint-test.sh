9d1ca2e0a708bf61f6273e614076dd5708dee5c9
petrus7 <piotr.dankowski@autea.eu>
2019-05-13 14:48:54 +0200

    fix test cript to not override error code

--- api/entrypoint-test.sh
+++ api/entrypoint-test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 /wait
-
+script_result=0
 celery -A api worker -l info &
 coverage run --source='.' manage.py test
-script_result=0
 
 if [ $? -ne 0 ]; then
     $script_result=$?
 fi
+
 coverage html
 coverage report
 exit $script_result
