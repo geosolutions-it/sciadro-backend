a92d34b1bc735278c53259de7dd53436ee576bcb
petrus7 <44587161+petrus7@users.noreply.github.com>
2019-05-28 10:23:10 +0000

    add endpoint for all anomalies in mission (#42)
    add pagination

--- api/service/urls.py
+++ api/service/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from django.urls import include
 
 from service.celery_views import VideoConversionStatus
-from .views import AssetViewSet, VideoStreamView, FrameViewSet, TelemetryViewSet, AnomalyViewSet
+from .views import AssetViewSet, VideoStreamView, FrameViewSet, TelemetryViewSet, AnomalyViewSet, \
+    AnomalyPerMissionViewSet
 from .views import MissionViewSet
 from rest_framework_nested.routers import DefaultRouter
 from rest_framework_swagger.views import get_swagger_view
@@ -16,7 +17,9 @@ router.register(r'assets/(?P<asset_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9
 router.register(r'assets/(?P<asset_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/missions/(?P<mission_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/video', VideoStreamView, basename='video')
 router.register(r'assets/(?P<asset_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/missions/(?P<mission_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/telemetry',TelemetryViewSet , basename='frame')
 router.register(r'assets/(?P<asset_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/missions/(?P<mission_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/frames', FrameViewSet, basename='telemetry')
+router.register(r'assets/(?P<asset_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/missions/(?P<mission_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/anomalies', AnomalyPerMissionViewSet, basename='mission_anomalies')
 router.register(r'assets/(?P<asset_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/missions/(?P<mission_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/frames/(?P<frame_uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/anomalies', AnomalyViewSet, basename='anomaly')
+
 
 
 schema_view = get_swagger_view(title='SCIADRO API')
